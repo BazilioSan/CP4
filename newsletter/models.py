698ca0bfb6b10bb9181eb1201b86d5dccaee5bbd
Alexandr <blix@list.ru>
2024-12-28 19:53:21 +0300

    + owner в NewsLetter

--- newsletter/models.py
+++ newsletter/models.py
@@ -86,6 +86,15 @@ class NewsLetter(models.Model):
     )
     recipient = models.ManyToManyField(Recipient, verbose_name="Получатель")
 
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Владелец",
+        related_name="newsletters",
+        blank=True,
+        null=True,
+    )
+
     def __str__(self):
         return self.status
 
